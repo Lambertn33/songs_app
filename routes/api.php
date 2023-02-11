aee2f28b1ace92a572f33d3adc06260e8ceaba71
Lambertn33 <lambertgithub@gmail.com>
2023-02-11 21:39:28 +0200

    feat: add albums

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AlbumsController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,4 +20,12 @@ Route::controller(AuthController::class)->group(function() {
   Route::post('login', 'login');
   Route::post('register', 'register');
   Route::post('logout', 'logout');
+});
+
+Route::controller(AlbumsController::class)->prefix('albums')->group(function() {
+  Route::get('/', 'getMyAlbums');
+  Route::post('/', 'createNewAlbum');
+  Route::get('/{id}', 'getSingleAlbum');
+  Route::put('/{id}', 'updateSingleAlbum');
+  Route::delete('/{id}', 'deleteSingleAlbum');
 });
